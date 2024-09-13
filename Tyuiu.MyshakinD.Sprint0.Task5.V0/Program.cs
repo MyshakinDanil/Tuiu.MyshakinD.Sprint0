59bf13a7fd5a235dd3ee82ad3c4780c62c7c6672
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-09-13 16:30:11 +0500

    Task5

---
+++ Tyuiu.MyshakinD.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MyshakinD.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.MyshakinD.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(8, 7));
+            Console.WriteLine(DataService.Substraction(15, 8));
+            Console.WriteLine(DataService.Multiplication(12, 12));
+            Console.WriteLine(DataService.Division(225, 15));
+
+            Console.WriteLine(DataService.Division(5, 0));
+
+            Console.ReadKey();
+        }
+    }
+}
